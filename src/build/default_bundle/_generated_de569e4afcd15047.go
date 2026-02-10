d0d3378d00c8457af770e5fa76f4ad732969f87d
DKislicyn <DKislicyn@beeline.kz>
2026-02-11 02:33:03 +0300

    fixes 3

---
+++ src/build/default_bundle/_generated_de569e4afcd15047.go
@@ -0,0 +1,13 @@
+components {
+  id: "main"
+  component: "/main/main.script"
+}
+embedded_components {
+  id: "background"
+  type: "sprite"
+  data: "default_animation: \"bg_paw\"\nmaterial: \"/builtins/materials/sprite.material\"\nsize {\n  x: 2247.0\n  y: 1264.0\n}\nsize_mode: SIZE_MODE_MANUAL\ntextures {\n  sampler: \"texture_sampler\"\n  texture: \"/assets/atlases/main.atlas\"\n}\n"
+  scale {
+    x: 1.1
+    y: 1.1
+  }
+}
